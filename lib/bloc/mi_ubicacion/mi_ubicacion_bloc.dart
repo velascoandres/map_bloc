6a6cddb49dac4d0950944b0ce85a5904470bd61b
andres <velasco.andrs@gmail.com>
2021-01-19 00:25:08 -0500

    seguir ruta del usuario

--- lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
+++ lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
 
@@ -10,6 +11,27 @@ part 'mi_ubicacion_state.dart';
 
 class MiUbicacionBloc extends Bloc<MiUbicacionEvent, MiUbicacionState> {
   MiUbicacionBloc() : super(MiUbicacionState());
+
+
+  StreamSubscription<Position> _positionSubscription;
+
+  void iniciarSeguimiendo() async {
+    // await Geolocator.isLocationServiceEnabled()
+
+   this._positionSubscription = Geolocator.getPositionStream(
+      desiredAccuracy: LocationAccuracy.high,
+      distanceFilter: 10, // emita luego de 10 metros
+    ).listen(
+      (Position position) {
+        print(position);
+      },
+    );
+  }
+
+  void cancelarSeguimiento(){
+    _positionSubscription?.cancel();
+  }
+
 
   @override
   Stream<MiUbicacionState> mapEventToState(
--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:map_bloc/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
 
-class MapaPage extends StatelessWidget {
+
+class MapaPage extends StatefulWidget {
   const MapaPage({Key key}) : super(key: key);
+
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  @override
+  void initState() {
+    
+    context.read<MiUbicacionBloc>().iniciarSeguimiendo();
+    
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    context.read<MiUbicacionBloc>().cancelarSeguimiento();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
