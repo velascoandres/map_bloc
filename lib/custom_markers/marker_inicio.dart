35dee0a3f8c26fa2ff2850fdc0096b826b3ad6e2
andres <velasco.andrs@gmail.com>
2021-01-31 10:34:52 -0500

    rectangulos en el custom painter

--- lib/custom_markers/marker_inicio.dart
+++ lib/custom_markers/marker_inicio.dart
@@ -6,7 +6,7 @@ class MarkerInicioPainter extends CustomPainter {
     Paint paint = new Paint()..color = Colors.black;
     // Dibujar circulo negro
     final double circuloNegroR = 20;
-    
+
     canvas.drawCircle(
       Offset(circuloNegroR, size.height - circuloNegroR),
       circuloNegroR,
@@ -21,6 +21,27 @@ class MarkerInicioPainter extends CustomPainter {
       ciculoBlancoR,
       paint,
     );
+    // Sombra
+    final path = new Path();
+    path.moveTo(40, 20);
+    path.lineTo(size.width - 10, 20);
+    path.lineTo(size.width - 10, 100);
+    path.lineTo(40, 100);
+    canvas.drawShadow(
+      path,
+      Colors.black87,
+      10,
+      false,
+    );
+
+    // Caja Blanca
+    final cajaBlanca = Rect.fromLTWH(40, 20, size.width - 55, 80);
+    canvas.drawRect(cajaBlanca, paint);
+
+    // Caja Negra
+    paint.color = Colors.black87;
+    final cajaNegra = Rect.fromLTWH(40, 20, 70, 80);
+    canvas.drawRect(cajaNegra, paint);
   }
 
   @override
